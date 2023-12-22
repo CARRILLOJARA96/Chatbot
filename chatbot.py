9488f2a272306fa9eaccf09ee1ef724ed5d72113
CARRILLOJARA96 <ernestocj_96@hotmail.com>
2023-12-21 23:18:30 -0500

    carga2

--- chatbot.py
+++ chatbot.py
@@ -6,6 +6,8 @@ load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 openai.api_key = api_key
 
+preguntas_anteriores = []
+respuestas_anteriores = []
 
 def preguntar_chat_gpt(prompt, modelo="gpt-3.5-turbo-instruct"):
     respuesta = openai.Completion.create(
@@ -21,13 +23,19 @@ def preguntar_chat_gpt(prompt, modelo="gpt-3.5-turbo-instruct"):
 print("Bienvenido a nuestro chatbot básico. Escribe 'salir' cuando quieras terminar")
 
 while True:
+    conversacion_historica = ""
     ingreso_usuario = input("\nTú:")
     if ingreso_usuario.lower() == "salir":
         break
 
-    prompt = f"El usuario pregunta: {ingreso_usuario}\nChatGPT responde:"
-    respuesta_gpt = preguntar_chat_gpt(prompt)
-    print(f"Chatbot: {respuesta_gpt}")
+    for pregunta, respuesta in zip(preguntas_anteriores, respuestas_anteriores):
+        conversacion_historica += f"El usuario pregunta: {pregunta}\n"
+        conversacion_historica += f"ChatGPT responde: {respuesta}\n"
 
-##nueva linea
-## segudna linea
+    prompt = f"El usuario pregunta: {ingreso_usuario}\n"
+    conversacion_historica += prompt
+    respuesta_gpt = preguntar_chat_gpt(conversacion_historica)
+    print(f"{respuesta_gpt}")
+
+    preguntas_anteriores.append(ingreso_usuario)
+    respuestas_anteriores.append(respuesta_gpt)
---
+++ crear_contenido.py
@@ -0,0 +1,8 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = api_key
+
